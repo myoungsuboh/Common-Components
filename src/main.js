cb542614f140ee227d73039b259d1fab16915e98
BOOK-PHM4Q6UO75\USER <po1202222@gmail.com>
2025-12-29 15:05:20 +0900

    [setting] : 다국어 처리, 라이브러리 추가

--- src/main.js
+++ src/main.js
@@ -1,21 +1,57 @@
-import App from './App.vue';
-import * as components from 'vuetify/components';
-import * as directives from 'vuetify/directives';
-import VueCodeHighlight from 'vue-code-highlight';
+import App from "./App.vue";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import VueCodeHighlight from "vue-code-highlight";
 
-import { createApp } from 'vue';
-import { createVuetify } from 'vuetify';
+import { createApp } from "vue";
+import { ko, en } from "vuetify/locale";
+import { createI18n, useI18n } from "vue-i18n";
+import { createVuetify } from "vuetify";
+import { createVueI18nAdapter } from "vuetify/locale/adapters/vue-i18n";
 
-import 'vuetify/styles';
-import './assets/main.css';
-import '@mdi/font/css/materialdesignicons.css';
+import "vuetify/styles";
+import "./assets/main.css";
+import "@mdi/font/css/materialdesignicons.css";
+
+const messages = {
+  en: {
+    $vuetify: {
+      ...en,
+      dataIterator: {
+        rowsPerPageText: "Items per page:",
+        pageText: "{0}-{1} of {2}",
+      },
+    },
+  },
+  ko: {
+    $vuetify: {
+      ...ko,
+      dataIterator: {
+        rowsPerPageText: "Element per sida:",
+        pageText: "{0}-{1} av {2}",
+      },
+    },
+  },
+};
+
+const i18n = createI18n({
+  legacy: false, // Vuetify does not support the legacy mode of vue-i18n
+  locale: "ko",
+  fallbackLocale: "ko",
+  messages,
+});
 
 const vuetify = createVuetify({
   components,
   directives,
+  locale: {
+    adapter: createVueI18nAdapter({ i18n, useI18n }),
+  },
 });
 
 const app = createApp(App);
+
+app.use(i18n);
 app.use(vuetify);
 app.use(VueCodeHighlight);
-app.mount('#app');
+app.mount("#app");
